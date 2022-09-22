20dd08a71429f200962895246dd515a3b8791021
naz664 <nazimnavas145@gmail.com>
2022-09-22 21:03:06 +0530

    Initial Patches

---
+++ README.md
@@ -0,0 +1,16 @@
+# Spark_patches
+Patches for building SparkOS gsi
+(WIP)
+
+## Credits
+These people have helped this project in some way or another, so they should be the ones who receive all the credit:
+- [SparkOS](https://github.com/Spark-Rom)
+- [Phhusson](https://github.com/phhusson)
+- [AndyYan](https://github.com/AndyCGYan)
+- [Ponces](https://github.com/ponces)
+- [Sooti](https://github.com/sooti)
+- [Haridhayal](https://github.com/haridhayal11)
+- [peter](https://gitea.angry.im/PeterGSI)
+
+
+# Download link :- [Here](https://sourceforge.net/projects/gsi-projects/files/A13/SparkOS/21092022/)
---
+++ Spark.mk
@@ -0,0 +1,17 @@
+$(call inherit-product, vendor/spark/config/common.mk)
+$(call inherit-product, vendor/spark/config/common_full.mk)
+$(call inherit-product, vendor/spark/config/BoardConfigSoong.mk)
+$(call inherit-product, vendor/spark/config/BoardConfigSpark.mk)
+$(call inherit-product, device/spark/sepolicy/common/sepolicy.mk)
+$(call inherit-product, vendor/spark/config/bootanimation.mk)
+-include vendor/spark/build/core/config.mk
+TARGET_NO_KERNEL_OVERRIDE := true
+TARGET_NO_KERNEL_IMAGE := true
+SELINUX_IGNORE_NEVERALLOWS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
+TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
+
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
