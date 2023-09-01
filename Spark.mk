c346f8b27d8bbe0e2579bfb4a66ccfea4b935eb0
naz664 <nazimnavas145@gmail.com>
2023-09-01 14:32:43 +0000

    feat: Sync with August source of Spark * Synced with latest td source
    Signed-off-by: naz664 <nazimnavas145@gmail.com>

--- Spark.mk
+++ Spark.mk
@@ -8,23 +8,6 @@ $(call inherit-product, vendor/spark/config/bootanimation.mk)
 TARGET_NO_KERNEL_OVERRIDE := true
 TARGET_NO_KERNEL_IMAGE := true
 SELINUX_IGNORE_NEVERALLOWS := true
-TARGET_BOOT_ANIMATION_RES := 1080
-
-TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
-TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
-
-TARGET_FACE_UNLOCK_SUPPORTED := true
-
-# Use CCache
-USE_CCACHE := true
-
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-	persist.sys.binary_xml=false \
-	dalvik.vm.dex2oat64.enabled=true \
-
-#prebuilts
-PRODUCT_PACKAGES += \
-  OpenEUICC \
 
 # Set Bootanimation at 720P
 TARGET_BOOT_ANIMATION_RES := 720
